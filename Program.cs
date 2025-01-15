0aa6f6c0cd4485c22414f84967288912c92f329a
osmanozysr <123673331+OsmanOzyasar@users.noreply.github.com>
2025-01-15 15:36:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace List2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> coffees = new List<string>();
+
+            Console.WriteLine("Sizden 5 adet kahve ismi istiyoruz. Lütfen ekrana yazınız.");
+
+            for (int i = 1; i < 6; i++)
+            {
+                Console.Write($"{i}. Kahve: ");
+                string userInput = Console.ReadLine();
+                coffees.Add(userInput);
+            }
+
+            Console.WriteLine("Girilen kahve isimleri: ");
+
+            foreach (string coffe in coffees)
+            {
+                Console.WriteLine(coffe);
+            }
+        }
+    }
+}
